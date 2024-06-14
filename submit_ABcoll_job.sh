f50c86140e77fb0401f3a5ab004f9fabba8c1398
Christopher Plumberg <astrophysicist87@gmail.com>
2024-06-14 12:43:16 -0700

    Add scripts for running on supercomputers

---
+++ submit_ABcoll_job.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# these values should correspond to available options in Trento
+nucleusA=$1
+nucleusB=$2
+nEvents=$3
+
+sbatch <<EOT
+#!/bin/bash
+#SBATCH -t 48:00:00
+#SBATCH --nodes=1
+#SBATCH --output="${nucleusA}${nucleusB}_job_%j.out"
+
+./build/src/trento ${nucleusA} ${nucleusB} ${nEvents}
+
+exit 0
+EOT
