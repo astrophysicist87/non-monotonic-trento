8447816a2c426ac83b5ca6a404db5aea22a78f44
Christopher Plumberg <astrophysicist87@gmail.com>
2024-06-14 11:40:57 -0700

    Update generate_initial_state_data.py
    Pass in binwidth as initial parameter, print out bin limits for diagnostic purposes

--- generate_initial_state_data.py
+++ generate_initial_state_data.py
@@ -1,6 +1,9 @@
 import numpy as np
 import sys
 
+bw          = sys.argv[1] # in %
+percentiles = np.linspace(0,100,1+100//bw)[1:-1]
+
 #====================================================================================
 def get_uptick(files):
     # columns for Npart and integrated S, respectively
@@ -9,11 +12,15 @@ def get_uptick(files):
     # sort by Npart
     data = data[data[:,0].argsort()]
     
-    # find Npart values corresponding to different percentiles
-    inds = np.digitize(data[:,0], np.percentile(data[:,0], np.linspace(10,90,9))[::-1])
+    # find indices corresponding to different percentiles
+    inds = np.digitize(data[:,0], np.percentile(data[:,0], percentiles)[::-1])
+    
+    # the Npart values at which the bins are split
+    binlimits = np.where(np.diff(inds))[0]+1
+    print('Bin limits:', binlimits)
     
     # split the data array accordingly
-    bins=np.split(data, np.where(np.diff(inds))[0]+1)
+    bins = np.split(data, binlimits)
 
     # evaluate and return the required observable in each bin as an array
     return np.array([[np.mean(bin[:,0]), 2.0*np.mean(bin[:,1])/np.mean(bin[:,0])] for bin in bins])
@@ -27,10 +34,14 @@ def get_eps2rms(files):
     data = data[data[:,0].argsort()]
     
     # find Npart values corresponding to different percentiles
-    inds = np.digitize(data[:,0], np.percentile(data[:,0], np.linspace(10,90,9))[::-1])
+    inds = np.digitize(data[:,0], np.percentile(data[:,0], percentiles)[::-1])
+    
+    # the Npart values at which the bins are split
+    binlimits = np.where(np.diff(inds))[0]+1
+    print('Bin limits:', binlimits)
     
     # split the data array accordingly
-    bins=np.split(data, np.where(np.diff(inds))[0]+1)
+    bins = np.split(data, binlimits)
     
     # evaluate and return the required observable in each bin as an array
     return np.array([[np.mean(bin[:,0]), np.sqrt(np.mean(bin[:,1]**2))] for bin in bins])
@@ -38,6 +49,6 @@ def get_eps2rms(files):
 
 #====================================================================================
 if __name__ == "__main__":
-    files = sys.argv[1:]
+    files = sys.argv[2:]
     print(get_uptick(files))
     #print(get_eps2rms(files))
