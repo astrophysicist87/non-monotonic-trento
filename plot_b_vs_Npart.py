08bfb36708b1c046e78bd1c58432f55253aa3242
Christopher Plumberg <astrophysicist87@gmail.com>
2024-07-01 10:13:06 -0700

    New binwidth in y direction

--- plot_b_vs_Npart.py
+++ plot_b_vs_Npart.py
@@ -6,7 +6,7 @@ data = np.loadtxt(sys.argv[1], usecols=(int(sys.argv[2]), int(sys.argv[3])))
 
 fig, ax = plt.subplots(nrows=1, ncols=1)
 
-bxw, byw = 0.1, 1.0
+bxw, byw = 0.1, 0.1
 nbx = int(np.amax(data[:,0])/bxw)
 nby = int(np.amax(data[:,1])/byw)
 ax.hist2d(data[:,0], data[:,1], bins=[nbx,nby])
